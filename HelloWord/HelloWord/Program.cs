588c6584a522810fd8d48b1a4f37c5a664800431
Huy Truong <truongkhaihuy1211@gmail.com>
2024-11-19 17:18:35 +0200

    HelloWord

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,40 @@
+﻿/*namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime now is : " + localDate);
+    }
+
+}*/
+/*namespace AreaRectangle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter height: ");
+        height = float.Parse(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Area is: " + area);
+    }
+}
+*/
+int i = 10;
+float f = 20.5f;
+double d = 20.5;
+Boolean b = true;
+char c = 'a';
+String s = "CodeGym";
+
+Console.WriteLine("Giá trị của biến i là " + i);
+Console.WriteLine("Giá trị của biến f là " + f);
+Console.WriteLine("Giá trị của biến d là " + d);
+Console.WriteLine("Giá trị của biến b là " + b);
+Console.WriteLine("Giá trị của biến c là " + c);
+Console.WriteLine("Giá trị của biến i là " + s);
